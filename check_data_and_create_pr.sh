a470c177fa2d07f15fe0c8b9108801aac95dd848
Dmitriy Kulagin <86959543+Dmitriy-Kulagin@users.noreply.github.com>
2023-03-23 16:39:45 +0300

    Update check_data_and_create_pr.sh

--- check_data_and_create_pr.sh
+++ check_data_and_create_pr.sh
@@ -9,6 +9,7 @@ python3 scripts/simple_data_check.py
 
 scripts/close_pr_if_exists.sh
 
-echo ${GITHUB_TOKEN} | gh auth login --with-token
+# echo ${GITHUB_TOKEN} | gh auth login --with-token
 
+export GH_TOKEN=${GITHUB_TOKEN}
 gh api -X POST /repos/tradingview-pine-seeds/${REPO_NAME}/pulls -f base="master" -f head="${REPO_OWNER}:${BRANCH_NAME}" -f title="Upload data"
