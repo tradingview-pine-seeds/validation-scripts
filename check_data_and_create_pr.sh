9d8d2a5077dabd798eb49d685dab9395b247789f
Dmitriy Kulagin <86959543+Dmitriy-Kulagin@users.noreply.github.com>
2023-03-28 11:58:26 +0300

    Update check_data_and_create_pr.sh

--- check_data_and_create_pr.sh
+++ check_data_and_create_pr.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+git clone "https://github.com/tradingview-pine-seeds/validation-scripts.git" scripts
+
 export TOKEN={GITHUB_TOKEN}
 bash scripts/validate_token.sh
 
+git clone "https://github.com/${REPO_OWNER}/${REPO_NAME}.git" .
+git checkout master
 PR_BRANCH_NAME="update_$(git log -n 1 --pretty=format:%H)"
 git checkout -b ${PR_BRANCH_NAME} > /dev/null 2>&1
-
 git push --set-upstream origin ${PR_BRANCH_NAME} > /dev/null 2>&1
 
 export GROUP=${REPO_NAME}
