5b1f25d71c3a9cf3b87cf1de5f7e32d63dc92e56
Dmitriy Kulagin <86959543+Dmitriy-Kulagin@users.noreply.github.com>
2023-03-28 12:23:19 +0300

    Update check_data_and_create_pr.sh

--- check_data_and_create_pr.sh
+++ check_data_and_create_pr.sh
@@ -4,16 +4,25 @@ set -e
 export TOKEN={GITHUB_TOKEN}
 bash scripts/validate_token.sh
 
-git clone "https://github.com/${REPO_OWNER}/${REPO_NAME}.git" .
+# checkout fork repo (via temp dir as current dir is not emply and it does't allow to check out repo in it)
+git clone "https://github.com/${REPO_OWNER}/${REPO_NAME}.git" temp
+mv temp/* .
+mv temp//git* .
+rmdir temp
+
+# create a new branch for update
 git checkout master
 PR_BRANCH_NAME="update_$(git log -n 1 --pretty=format:%H)"
 git checkout -b ${PR_BRANCH_NAME} > /dev/null 2>&1
 git push --set-upstream origin ${PR_BRANCH_NAME} > /dev/null 2>&1
 
+# check data in update
 export GROUP=${REPO_NAME}
 python3 scripts/simple_data_check.py
 
+# close previous PR if it exists
 scripts/close_pr_if_exists.sh
 
+# create new PR
 export GH_TOKEN=${GITHUB_TOKEN}
 gh api -X POST /repos/tradingview-pine-seeds/${REPO_NAME}/pulls -f base="master" -f head="${REPO_OWNER}:${PR_BRANCH_NAME}" -f title="Upload data"
