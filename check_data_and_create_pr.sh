a09d9c28bd3fcfe7f491e565d12ec49e031d2cfb
dpoliakov <dpoliakov@tradingview.com>
2023-10-18 17:09:15 +0300

    DF-2333: debug script on GH runner done

--- check_data_and_create_pr.sh
+++ check_data_and_create_pr.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
+# setted by GitHub
 set -e
+set +o pipefail
 
 bash scripts/validate_token.sh
 
@@ -9,13 +12,25 @@ mv temp/* .
 mv temp//.git* .
 rmdir temp
 
-
+set +e
+set +o pipefail
 # remove unused branches before creating new
 git checkout master
-# delete merged and unmerged remote branches
-git branch --merged | egrep -v "(^\*|master)" | xargs -I % sh -c 'git branch -D %; git push --delete origin %';
-git branch --no-merged | egrep -v "(^\*|master)" | xargs -I % sh -c 'git branch -D %; git push --delete origin %';
+git branch --list | cat
+merged_branches=$(git branch -r --merged | grep "update_*" -c)
+nomerged_branches=$(git branch --no-merged | grep "update_*" -c)
+total_branches=$(($merged_branches+$nomerged_branches))
 
+# delete all merged and unmerged remote `update_*` branches
+if [[ $total_branches > 0 ]]
+then
+    git branch -r --merged | grep "update_*" | cut -d "/" -f 2 | xargs git push --delete origin
+    git branch --no-merged | grep "update_*" | xargs -I % bash -c 'git branch -D %; git push --delete origin %';
+else
+    echo "No temporary branch to remove"
+fi
+
+set -e
 # create a new branch for update
 git checkout master
 PR_BRANCH_NAME="update_$(git log -n 1 --pretty=format:%H)"
@@ -30,5 +45,6 @@ python3 scripts/simple_data_check.py
 bash scripts/close_pr_if_exists.sh
 
 # create new PR
+set +e
 export GH_TOKEN=${ACTION_TOKEN}
 gh api -X POST /repos/tradingview-pine-seeds/${REPO_NAME}/pulls -f base="master" -f head="${REPO_OWNER}:${PR_BRANCH_NAME}" -f title="Upload data" > /dev/null 2>&1
