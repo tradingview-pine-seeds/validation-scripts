ca67e14d3cec1db280eca307ec7ac57d45868a00
dpoliakov <dpoliakov@tradingview.com>
2023-10-27 15:16:19 +0300

    DF-2472: first itteration

--- check_data_and_create_pr.sh
+++ check_data_and_create_pr.sh
@@ -9,6 +9,22 @@ mv temp/* .
 mv temp//.git* .
 rmdir temp
 
+
+# check GH PR limit 3k files changing
+# if [[ -z "${FROM_COMMIT}" ]]; then
+#     FROM_COMMIT=$(git rev-list --max-parents=0 HEAD)
+#     TO_COMMIT="HEAD"
+# fi
+
+git config diff.renameLimit 999999
+#CHANGED_DATA_FILES=$(git diff --name-only --diff-filter=AM "$FROM_COMMIT".."$TO_COMMIT" | grep csv)
+CHANGED_DATA_FILES=${git diff --name-only ${{ github.event.before }} ${{ github.event.after }} | wc -l}
+if [["$CHANGED_DATA_FILES" -gt 3000 ]]; then
+    echo "More then 3000 files added/changed. Please, push commit with changes less than in 3000 files"
+    exit(1)
+fi
+
+
 # create a new branch for update
 git checkout master
 PR_BRANCH_NAME="update_$(git log -n 1 --pretty=format:%H)"
